b343bfb8ac0a7a991af26561e8e7c41da53022f4
woosungchu <woosungchu@gmail.com>
2017-07-23 02:26:44 +0900

    connect project with firebase

--- README.md
+++ README.md
@@ -18,4 +18,5 @@ npm run build
 npm run build --report
 ```
 
-For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+# Firebase Authorization
+https://stackoverflow.com/questions/35418143/how-to-restrict-firebase-data-modification
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,104 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+  <div class="page-header">
+      <h1>Vue.js 2 & Firebase <small>Sample Application by CodingTheSmartWay.com</small></h1>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">Add New Books</h3>
+      </div>
+      <div class="panel-body">
+         <form id="form" class="form-inline" v-on:submit.prevent="addBook">
+          <div class="form-group">
+            <label for="bookTitle">Title:</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          </div>
+          <div class="form-group">
+            <label for="bookAuthor">Author:</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          </div>
+          <div class="form-group">
+            <label for="bookUrl">Url:</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Book">
+        </form>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">Book List</h3>
+      </div>
+      <div class="panel-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="book in books">
+              <td><a v-bind:href="book.url">{{book.title}}</a></td>
+              <td>{{book.author}}</td>
+              <td><span class="glyphicon glyphicon-trash" aria-hidden="true" v-on:click="removeBook(book)"></span></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase'
+
+let config = {
+  apiKey: "AIzaSyATJF0TthZFfzONJ1ZaoO13PjWZqJYYU8M",
+  authDomain: "fir-vue-tutorial.firebaseapp.com",
+  databaseURL: "https://fir-vue-tutorial.firebaseio.com",
+  projectId: "fir-vue-tutorial",
+  storageBucket: "fir-vue-tutorial.appspot.com",
+  messagingSenderId: "8930388054"
+};
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let booksRef = db.ref('books')
+
 export default {
   name: 'hello',
+  firebase: {
+    books: booksRef
+  },
+
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newBook: {
+          title: '',
+          author: '',
+          url: 'http://'
+      }
     }
-  }
+  },
+
+   methods: {
+      addBook: function () {
+        booksRef.push(this.newBook);
+        this.newBook.title = '';
+        this.newBook.author = '';
+        this.newBook.url = 'http://';
+      },
+      removeBook: function (book) {
+        booksRef.child(book['.key']).remove()
+        toastr.success('Book removed successfully')
+      }
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
